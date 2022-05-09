6c23ba186d56044824931b0bd68e457225af63ea
Ivo Matavele <mataveleivo@gmail.com>
2022-05-09 23:14:50 +0200

    Inicio do projecto

---
+++ backend/src/test/java/co/mz/matavele/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mz.matavele.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
